5790337f20cee1b13b2676b225431b6b87e5fa3f
vinni-905 <likhithachinthirala@gmail.com>
2025-07-14 11:47:52 +0530

    Completed Fullstack Open Part 2 exercises 2.6 to 2.11 with JSON server integration

--- src/App.jsx
+++ src/App.jsx
@@ -1,67 +1,71 @@
-import { useState } from 'react';
-import Filter from './components/Filter';
-import PersonForm from './components/PersonForm';
-import Persons from './components/Persons';
+import { useState, useEffect } from 'react'
+import personsService from './services/persons'
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456', id: 1 }
-  ]);
-  const [newName, setNewName] = useState('');
-  const [newNumber, setNewNumber] = useState('');
-  const [filter, setFilter] = useState('');
+  const [persons, setPersons] = useState([])
+  const [newName, setNewName] = useState('')
+  const [newNumber, setNewNumber] = useState('')
+
+  useEffect(() => {
+    personsService
+      .getAll()
+      .then(initialPersons => {
+        setPersons(initialPersons)
+      })
+  }, [])
 
   const addPerson = (event) => {
-    event.preventDefault();
+    event.preventDefault()
 
+    // Check if the name already exists
     if (persons.some(person => person.name === newName)) {
-      alert(`${newName} is already added to phonebook`);
-      return;
+      alert(`${newName} is already added to phonebook`)
+      return
     }
 
-    const personObject = {
+    const newPerson = {
       name: newName,
       number: newNumber,
-      id: persons.length + 1,
-    };
+    }
 
-    setPersons(persons.concat(personObject));
-    setNewName('');
-    setNewNumber('');
-  };
+    personsService
+      .create(newPerson)
+      .then(returnedPerson => {
+        setPersons(persons.concat(returnedPerson))
+        setNewName('')
+        setNewNumber('')
+      })
+  }
 
-  const handleNameChange = (event) => setNewName(event.target.value);
-  const handleNumberChange = (event) => setNewNumber(event.target.value);
-  const handleFilterChange = (event) => setFilter(event.target.value);
-
-  // Filter persons to show based on filter input (case insensitive)
-  const personsToShow = filter === ''
-    ? persons
-    : persons.filter(person => 
-        person.name.toLowerCase().includes(filter.toLowerCase())
-      );
+  const handleNameChange = (event) => setNewName(event.target.value)
+  const handleNumberChange = (event) => setNewNumber(event.target.value)
 
   return (
     <div>
       <h2>Phonebook</h2>
-      
-      <Filter filter={filter} handleFilterChange={handleFilterChange} />
 
-      <h3>Add a new</h3>
-      
-      <PersonForm 
-        addPerson={addPerson} 
-        newName={newName} 
-        handleNameChange={handleNameChange} 
-        newNumber={newNumber} 
-        handleNumberChange={handleNumberChange} 
-      />
+      <form onSubmit={addPerson}>
+        <div>
+          name: <input value={newName} onChange={handleNameChange} />
+        </div>
+        <div>
+          number: <input value={newNumber} onChange={handleNumberChange} />
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
 
-      <h3>Numbers</h3>
-      
-      <Persons personsToShow={personsToShow} />
+      <h2>Numbers</h2>
+      <ul>
+        {persons.map(person => 
+          <li key={person.id}>
+            {person.name} {person.number}
+          </li>
+        )}
+      </ul>
     </div>
-  );
-};
+  )
+}
 
-export default App;
+export default App
